1ae897d1db9d2a8a0b6f16e75a8d9ab1f9abf133
ungureanudaniel <ungureanu.daniel86@gmail.com>
2026-02-12 17:00:43 +0200

    Fixed docker

--- server_automate_main/urls.py
+++ server_automate_main/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
+
+def health_check(request):
+    return HttpResponse('Health OK', content_type='text/plain')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', health_check),
     path('api/', include('api.urls')),
     path('', include('monitoring.urls')),
     path('parameters/', include('parameters.urls')),
