0d7e2078f8608f00657b1e3f904be97f68a02d6e
ungureanudaniel <ungureanu.daniel86@gmail.com>
2026-02-13 14:44:42 +0200

    translatiojn

--- parameters/admin.py
+++ parameters/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import Produs, Serie, Tranzactie, PosPaper
+from .models import Produs, Serie, Tranzactie, PosPaper, TicketMachine
 
 @admin.register(Produs)
 class ProdusAdmin(admin.ModelAdmin):
@@ -23,3 +23,9 @@ class PosPaperAdmin(admin.ModelAdmin):
     list_display = ('pos_id', 'tickets_at_last_change', 'roll_capacity', 'last_change')
     list_filter = ('pos_id', 'last_change')
     readonly_fields = [f.name for f in PosPaper._meta.fields]
+
+@admin.register(TicketMachine)
+class TicketMachineAdmin(admin.ModelAdmin):
+    list_display = ('pos_id', 'ip_address', 'is_online', 'last_online')
+    list_filter = ('is_online', 'last_online')
+    readonly_fields = [f.name for f in TicketMachine._meta.fields]
