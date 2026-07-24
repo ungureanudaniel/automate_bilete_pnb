4e3cffe13e278eb7090a0121c7cfa240045e2c87
ungureanudaniel <ungureanu.daniel86@gmail.com>
2026-07-24 13:07:20 +0300

    fixed sales details

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import PosMachine
+
+@admin.register(PosMachine)
+class PosMachineAdmin(admin.ModelAdmin):
+    list_display = ('pos_id', 'name', 'ip_address', 'is_online', 'last_seen')
+    search_fields = ('pos_id', 'name', 'ip_address')
+    list_filter = ('last_seen',)
